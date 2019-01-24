85e9c7ad44801c56960070352d548768344455ff
J. Singleton <jennifer.singleton@gmail.com>
2019-01-24 01:17:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.collect do |anagram|
+      
+    end
+  end
+
+end
